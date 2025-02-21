bd23e2d931fcb26fabfe072f1d69b08a0a4e46e4
Matheus Maia Ribeiro <matheussmaiatech@gmail.com>
2025-02-21 10:49:15 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const modal = document.querySelector('.modal')
+const mascara = document.querySelector('.mascara-modal')
+
+function mostrarModal() {
+    modal.style.top = '50%'
+    mascara.style.visibility = 'visible'
+}
+
+function esconderModal() {
+    modal.style.top = '150%'
+    mascara.style.visibility = 'hidden'
+}
