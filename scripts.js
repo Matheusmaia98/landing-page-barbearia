4f76d7627eb940c96bc0d3016d8ee07d57d23093
Matheus Maia Ribeiro <matheussmaiatech@gmail.com>
2025-02-21 12:38:21 -0300

    testando responsividade

--- scripts.js
+++ scripts.js
@@ -10,3 +10,15 @@ function esconderModal() {
     modal.style.top = '150%'
     mascara.style.visibility = 'hidden'
 }
+
+function trocarVideo() {
+    const video = document.querySelector('.video-pc');
+    const source = document.getElementById("video-source");
+    const largura = window.innerWidth;
+
+    if (largura <= 768) {
+        source.src = "videos/video-mobile.mp4";
+    } else {
+        source.src = "videos/barber-2.mp4"
+    }
+}
